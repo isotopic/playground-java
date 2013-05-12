86292c9627ad70e8e8c4acc8f5b873008092c66b
guilhermecruz@gmail.com <guilhermecruz@gmail.com@f1600684-6fb8-ff8a-611d-4e8cb21cc822>
2013-05-12 20:09:46 +0000

    Commit from sublime text 2.

--- Code.java
+++ Code.java
@@ -2,7 +2,7 @@ public class Code{
 
 	public static void main(String []args){
 
-		System.out.println("The parameter is now "+args[0]);
+		System.out.println("Passed parameter was "+args[0]);
 
 
 	}
